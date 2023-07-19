89e51cf1bb01023656d76a74f87092310bf3c940
MalachyCode <nwaformalachy2@gmail.com>
2023-07-19 11:47:54 +0100

    Several changes made to the backend of the app. Also implementation of connection to MongoDB was done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json());
 app.use(express.static('build'));
+app.use(express.json());
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
@@ -16,77 +17,118 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
+const Note = require('./models/note');
+const password = process.argv[2];
+
 let notes = [
   {
-    id: 1,
+    // id: 1,
     content: 'HTML is easy',
     important: true,
   },
   {
-    id: 2,
+    // id: 2,
     content: 'Browser can execute only JavaScript',
     important: false,
   },
   {
-    id: 3,
+    // id: 3,
     content: 'GET and POST are the most important methods of HTTP protocol',
     important: true,
   },
   {
-    id: 4,
+    // id: 4,
     content: 'GET is a method of HTTP protocol',
     important: true,
   },
 ];
+
+// GENERATE NEW NOTE
+// for (let i = 0; i < notes.length - 1; i++) {
+//   console.log(notes[i].content);
+//   const note = new Note({
+//     content: notes[i].content,
+//     important: notes[i].important,
+//   });
+
+//   // SAVE GENERATED NOTE TO DATABASE
+//   note.save().then((result) => {
+//     console.log('note saved!');
+//     mongoose.connection.close();
+//   });
+// }
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>');
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body;
 
-app.post('/api/notes', (request, response) => {
+  // const note = {
+  //   content: body.content,
+  //   important: body.important,
+  // };
+
+  // Note.findByIdAndUpdate(request.params.id, note, { new: true });
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, content: 'query' }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(404).json({
-      error: 'content missing',
-    });
-  }
-  const note = {
+  // if (body.content === undefined) {
+  //   return response.status(404).json({
+  //     error: 'content missing',
+  //   });
+  // }
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
+  // notes = notes.concat(note);
 
-  response.json(note);
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -94,6 +136,21 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
